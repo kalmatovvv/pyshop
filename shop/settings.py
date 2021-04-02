bdfe8c9f918f8c1dfe66af43fa023d06b8c0a995
hello <kalmatovs03@gmail.com>
2021-04-02 16:55:46 +0600

    shop

--- shop/settings.py
+++ shop/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['demo-shamil-pyshop.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['demo-shamil-shop.herokuapp.com', 'localhost']
 
 
 # Application definition
