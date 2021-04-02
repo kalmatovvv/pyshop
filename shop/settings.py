151e9e550e538776bbd7c01bccefa4c367ebd470
hello <kalmatovs03@gmail.com>
2021-04-02 17:28:53 +0600

    settings

--- shop/settings.py
+++ shop/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = ['demo-shama-pyshop.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['demo-shamil-pyshop.herokuapp.com', 'localhost']
 
 
 # Application definition
